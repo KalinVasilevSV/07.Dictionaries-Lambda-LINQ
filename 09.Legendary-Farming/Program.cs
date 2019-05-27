ac1d3a970dea9d4e03fd18df8342f8c9008df5c4
KalinVasilevSV <kalinsv@abv.bg>
2019-05-28 00:19:45 +0300

    work on 28.05

---
+++ 09.Legendary-Farming/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _09.Legendary_Farming
+{
+    //Completed
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> inventory = new Dictionary<string, int>();
+            inventory["shards"] = 0;
+            inventory["fragments"] = 0;
+            inventory["motes"] = 0;
+            while (true) {
+                string[] input = Console.ReadLine().Trim().ToLower().Split().ToArray();
+
+                for (int i = 1; i < input.Length; i+=2) {
+
+                    if (!inventory.ContainsKey(input[i])) {
+                        inventory[input[i]] = 0;
+                    }
+                    inventory[input[i]] += int.Parse(input[i - 1]);
+
+                    if (inventory["shards"] >= 250) {
+                        inventory["shards"] -= 250;
+                        Console.WriteLine("Shadowmourne obtained!"); goto LegendaryObtained;
+                    }
+                    else if (inventory["fragments"] >= 250) {
+                        inventory["fragments"] -= 250;
+                        Console.WriteLine("Valanyr obtained!"); goto LegendaryObtained;
+                    }
+                    else if (inventory["motes"] >= 250) {
+                        inventory["motes"] -= 250;
+                        Console.WriteLine("Dragonwrath obtained!"); goto LegendaryObtained;
+                    }
+                }
+
+                
+            }
+            LegendaryObtained:;
+            
+            // Following Dicionaries are the inventory split in two
+            //inventory.Where() requires ToDictionary as well. The Dictionary constructor reqires a parameter of type Dictionary
+            Dictionary<string, int> legendaryMaterials = new Dictionary<string, int>(inventory.Where(item => item.Key == "shards" || item.Key == "fragments" || item.Key == "motes").ToDictionary(keySelector: item => item.Key, elementSelector: item => item.Value));
+            legendaryMaterials = legendaryMaterials.OrderByDescending(item => item.Value).ThenBy(item=>item.Key).ToDictionary(keySelector:item=>item.Key,elementSelector:item=>item.Value);
+
+            // Could use SortedDictionary for remainingMaterials but that would preclude any other sorting options
+            Dictionary<string,int> remainingMaterials =new Dictionary<string, int>(inventory.Where(item => item.Key != "shards" && item.Key != "fragments" && item.Key != "motes").ToDictionary(keySelector: item => item.Key, elementSelector: item => item.Value));
+            remainingMaterials = remainingMaterials.OrderBy(item => item.Key).ToDictionary(keySelector:item => item.Key, elementSelector:item =>item.Value);
+
+            // Prints the result
+            foreach(KeyValuePair<string,int> item in legendaryMaterials) {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
+            foreach (KeyValuePair<string, int> item in remainingMaterials) {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
+        }
+    }
+}
---
+++ 10.Srabsko-Unleashed/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _10.Srabsko_Unleashed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string,long>> singerProfitsByVenue = new Dictionary<string, Dictionary<string,long>>();
+
+            while (true) {
+
+                string input = Console.ReadLine();
+                if (input == "End") break;
+
+                string singerName = input.Substring(0, input.IndexOf('@'));
+                if (!singerName.EndsWith(" ")) continue; //Judge fails at compile time if EndsWith is give type char but works fine on my machine
+                else singerName = singerName.Trim();
+
+                string[] concert = input.Substring(input.IndexOf('@')+1).Split();
+                long ticketsCount;
+                long ticketsPrice;
+                try {
+                    ticketsCount = long.Parse(concert[concert.Length - 2]);
+                    ticketsPrice = long.Parse(concert[concert.Length - 1]);
+                }
+                catch (Exception) {
+                    //NO longer needed to access the message of the exception
+                    //Possible exception in this task were IndexOutOfRangeException and FormatException
+                    //Console.WriteLine(e.Message);
+                    continue;
+                }
+
+                string venueName = String.Join(" ",concert.Take(concert.Length - 2));
+
+                if (!singerProfitsByVenue.ContainsKey(venueName)) {
+                    singerProfitsByVenue[venueName] = new Dictionary<string, long>();
+                }
+
+                if (!singerProfitsByVenue[venueName].ContainsKey(singerName)) {
+                    singerProfitsByVenue[venueName][singerName] = (ticketsCount * ticketsPrice);
+                }
+                else {
+                    singerProfitsByVenue[venueName][singerName] += (ticketsCount * ticketsPrice);
+                }
+            }
+
+            Dictionary<string, Dictionary<string, long>> results = new Dictionary<string, Dictionary<string, long>>();
+
+            foreach (KeyValuePair<string,Dictionary<string,long>> venue in singerProfitsByVenue) {
+                results[venue.Key] = venue.Value.OrderByDescending(singer => singer.Value).ToDictionary(keySelector:singer=>singer.Key,elementSelector:singer=>singer.Value);
+            }
+
+            foreach (KeyValuePair<string,Dictionary<string,long>> venue in results) {
+                Console.WriteLine($"{venue.Key}");
+                foreach(KeyValuePair<string,long> singer in venue.Value) {
+                    Console.WriteLine($"#  {singer.Key} -> {singer.Value}");
+                }
+            }
+        }
+
+    }
+}
